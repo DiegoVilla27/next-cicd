ab91088d4477ca321abeaad7af93c557c6943417
Diego Villa <dv270992@gmail.com>
2024-09-01 11:03:55 +0200

    feat: Signed text with branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>
-            src/app/page.tsx{" "}
-            {/* {process.env.NODE_ENV === "development" ? "DEVELOP" : "PRODUCTION"} */}
-          </code>
+          <code className={styles.code}>src/app/page.tsx CICD</code>
         </p>
         <div>
           <a
